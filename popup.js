1e0d6ef65b6b73a92cd40eeffe6d689a90c282dc
Niccolò Marcon <marcon.niccolo@gmail.com>
2015-10-16 18:57:40 +0200

    clean the code

--- popup.js
+++ popup.js
@@ -9,6 +9,7 @@ function callApi(url, callback, fallback) {
   mXmlHttp.open('GET', api + enUrl + '&strategy=mobile', true);
   dXmlHttp.open('GET', api + enUrl + '&strategy=desktop', true);
 
+  // Setting the request to start the second one if gets the data
   mXmlHttp.onreadystatechange = function() {
     if (mXmlHttp.readyState == 4 && mXmlHttp.status == 200) {
       var response = JSON.parse(mXmlHttp.responseText);
@@ -19,6 +20,7 @@ function callApi(url, callback, fallback) {
     }
   };
 
+  // Calls the callback sending the points
   dXmlHttp.onreadystatechange = function() {
     if (dXmlHttp.readyState == 4 && dXmlHttp.status == 200) {
       var response = JSON.parse(dXmlHttp.responseText);
@@ -31,16 +33,19 @@ function callApi(url, callback, fallback) {
     }
   };
 
+  // Kick off the first request
   mXmlHttp.send();
 }
 
 // Get the current tab URL using chrome extnsion's APIs
 function getCurrentTabUrl(callback) {
+  // Setting the query options
   var queryInfo = {
     active: true,
     currentWindow: true,
   };
 
+  // Get the current tab and calling the callback
   chrome.tabs.query(queryInfo, function(tabs) {
     var tab = tabs[0];
     var url = tab.url;
@@ -53,58 +58,59 @@ function printResult(res) {
   var thresholdGood = 85; var thresholdWarn = 65;
   var green = '#009a2d'; var yellow = '#fda100'; var red = '#dd4b39';
 
-  var mColor; var mClass; var dColor; var dClass;
+  // Gets the html elements
+  var mPoints = document.getElementById('mobilePoints');
+  var mIcon   = document.getElementById('mobileIcon');
+  var dPoints = document.getElementById('desktopPoints');
+  var dIcon   = document.getElementById('desktopIcon');
 
-  // Check results for mobile
-  if (res.mobile >= thresholdGood) {
-    mColor = green;
-    mClass = ' mobile_excellent';
-  } else if (res.mobile >= thresholdWarn) {
-    mColor = yellow;
-    mClass = ' mobile_warning';
-  } else {
-    mColor = red;
-    mClass = ' mobile_error';
-  }
-
-  // Check results for desktop
-  if (res.desktop >= thresholdGood) {
-    dColor = green;
-    dClass = ' desktop_excellent';
-  } else if (res.desktop >= thresholdWarn) {
-    dColor = yellow;
-    dClass = ' desktop_warning';
-  } else {
-    dColor = red;
-    dClass = ' desktop_error';
-  }
-
-  // Show the results
-  mPoints = document.getElementById('mobilePoints');
-  mIcon = document.getElementById('mobileIcon');
-  dPoints = document.getElementById('desktopPoints');
-  dIcon = document.getElementById('desktopIcon');
-
+  // Display the scores
   mPoints.innerHTML = res.mobile;
-  mPoints.style.background = mColor;
-  mIcon.className += mClass;
-
   dPoints.innerHTML = res.desktop;
-  dPoints.style.background = dColor;
-  dIcon.className += dClass;
+
+  // Select the right class and color
+  switch (true) {
+    case(res.mobile >= thresholdGood):
+      mPoints.style.background = green;
+      mIcon.className += ' mobile_excellent';
+      break;
+    case(res.mobile >= thresholdWarn):
+      mPoints.style.background = yellow;
+      mIcon.className += ' mobile_warning';
+      break;
+    default:
+      mPoints.style.background = red;
+      mIcon.className += ' mobile_error';
+  }
+
+  switch (true) {
+    case(res.desktop >= thresholdGood):
+      dPoints.style.background = green;
+      dIcon.className += ' desktop_excellent';
+      break;
+    case(res.desktop >= thresholdWarn):
+      dPoints.style.background = yellow;
+      dIcon.className += ' desktop_warning';
+      break;
+    default:
+      dPoints.style.background = red;
+      dIcon.className += ' desktop_error';
+  }
 
   display(0, 0, 1);
 }
 
 function display(error, loading, data) {
-  document.getElementById('error').style.display   = error ? 'block' : 'none';
+  document.getElementById('error').style.display   = error   ? 'block' : 'none';
   document.getElementById('loading').style.display = loading ? 'block' : 'none';
-  document.getElementById('data').style.display    = data ? 'block' : 'none';
+  document.getElementById('data').style.display    = data    ? 'block' : 'none';
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   // Load URL tab
   getCurrentTabUrl(function(url) {
+
+    // Set the show more button with the user settings
     document.getElementById('show').addEventListener('click', function() {
       chrome.storage.sync.get(defaultOptions, function(item) {
         if (item.psi) {
@@ -113,10 +119,14 @@ document.addEventListener('DOMContentLoaded', function() {
         }
       });
     });
+
+    // Set the onclick event of the link to start a new request
     document.getElementById('tryAgain').onclick = function() {
       display(0, 1, 0);
       callApi(url, printResult, display);
     };
+
+    // Kick off the first request
     callApi(url, printResult, display);
   });
 });
